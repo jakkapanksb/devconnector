9bc4b27143da0b9685272818dea02171e7b48771
tatarksb <tatarksb@windowslive.com>
2018-06-29 12:28:19 +0700

    Add GET, POST and Validation api/profile

--- validation/is-empty.js
+++ validation/is-empty.js
@@ -1,8 +1,10 @@
 const isEmpty = value => {
-  value === undefined ||
+  return (
+    value === undefined ||
     value === null ||
-    (typeof value === "Object" && Object.keys(value).length === 0) ||
-    (typeof value === "string" && value.trim().length === 0);
+    (typeof value === "object" && Object.keys(value).length === 0) ||
+    (typeof value === "string" && value.trim().length === 0)
+  );
 };
 
 module.exports = isEmpty;
