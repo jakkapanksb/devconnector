76f269bde8fc7c73bcd3cc48b04c1ecb58968018
tatarksb <tatarksb@windowslive.com>
2018-07-12 17:50:49 +0700

    Add GET and DELETE /api/posts route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,6 +5,8 @@ const passport = require("passport");
 
 // Post model
 const Post = require("../../models/Post");
+// Profile model
+const Profile = require("../../models/Profile");
 
 // Validation
 const validatePostInput = require("../../validation/post");
@@ -13,6 +15,26 @@ const validatePostInput = require("../../validation/post");
 // @desc    Tests post route
 // @access  public
 router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
+
+// @route   GET api/posts/
+// @desc    Get posts
+// @access  Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json(err));
+});
+
+// @route   GET api/posts/
+// @desc    Get posts
+// @access  Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json(err));
+});
 
 // @route   POST api/posts
 // @desc    Create post
@@ -36,6 +58,31 @@ router.post(
     });
 
     newPost.save().then(post => res.json(post));
+  }
+);
+
+// @route   DELETE api/posts/:id
+// @desc    Delete post
+// @access  Private
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          // Check for psot owner
+          if (post.user.toString() !== req.user.id) {
+            return res
+              .status(401)
+              .json({ notauthorized: "User not authorized" });
+          }
+
+          // Delete
+          post.remove().then(() => res.json({ succes: true }));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+    });
   }
 );
 
