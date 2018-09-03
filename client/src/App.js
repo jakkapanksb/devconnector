024122bce69e4e4f37a01876edbe6c5740f0d34f
tatarksb <tatarksb@windowslive.com>
2018-09-03 14:27:12 +0700

    Setup React from beginning and Landing page

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Navbar from "./component/layout/Navbar";
+import Footer from "./component/layout/Footer";
+import Landing from "./component/layout/Landing";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Landing />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
