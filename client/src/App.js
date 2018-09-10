51d3d3dba5b03f082537af82af5eafba0da4e83b
tatarksb <tatarksb@windowslive.com>
2018-09-10 14:17:22 +0700

    Some setup react redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,32 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+
 import Navbar from "./component/layout/Navbar";
 import Footer from "./component/layout/Footer";
 import Landing from "./component/layout/Landing";
+import Register from "./component/auth/Register";
+import Login from "./component/auth/Login";
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,18 @@
+import { TEST_DISPATCH } from "../action/types";
+
+const initialState = {
+  isAuthenticated: false,
+  user: {}
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case TEST_DISPATCH:
+      return {
+        ...state,
+        user: action.payload
+      };
+    default:
+      return state;
+  }
+}
